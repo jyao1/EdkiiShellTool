64a837587813d880c440a0977b9a9e924d67abef
Jiewen Yao <jiewen.yao@intel.com>
2020-04-09 07:13:53 +0800

    Add RTCode Allocation in DummyRt

--- EdkiiShellToolPkg/DummyRt/DummyRt.c
+++ EdkiiShellToolPkg/DummyRt/DummyRt.c
@@ -7,9 +7,11 @@
 
 #include <Uefi.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 
 VOID *mRtData;
+VOID *mRtCode;
 
 /**
   Main entry for this driver/library.
@@ -25,9 +27,12 @@ TestEntryPoint (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  mRtData = AllocateRuntimePool (1);
   Print (L"RT .code - 0x%p\n", TestEntryPoint);
   Print (L"RT .data - 0x%p\n", &mRtData);
+
+  gBS->AllocatePool (EfiRuntimeServicesCode, 1, &mRtCode);
+  gBS->AllocatePool (EfiRuntimeServicesData, 1, &mRtData);
+  Print (L"RT Code - 0x%p\n", mRtCode);
   Print (L"RT Data - 0x%p\n", mRtData);
   return EFI_SUCCESS;
 }
